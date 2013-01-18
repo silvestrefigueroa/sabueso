d7c5140ab3acf15232359a6f276f49c18d7e6b72
silvestrefigueroa@gmail.com <silvestrefigueroa@gmail.com@037a9b29-60ad-6186-cfe9-0938512c29f3>
2013-01-18 00:28:01 +0000

    bien, ahora obtengo los datos del mensaje arp y de la trama, ya estoy listo para comparar y para guardar y para hacer lo que quiera con ellos, YA SE COMO MANIPULARLOS wiiiiiiii. Continuar con el diseño e implementacion del almacenado de estos./sabueso sabueso.conf

--- sabueso.c
+++ sabueso.c
@@ -42,14 +42,29 @@ void my_callback(u_char *useless,const struct pcap_pkthdr* pkthdr,const u_char* 
 		//muestre datos de la captura:
 	struct ether_header* eptr;
 	eptr = (struct ether_header*) packet;//apunta a la cabecera ethernet (casteado a ethernet)
+	printf("-------------------------------------------------------------------------------------------------------------------\n");
 	printf("Paquete numero: %d\n",count);
 	//printf("MAC origen en la TRAMA ETHERNET: %s\n", ether_ntoa(eptr−>ether_shost));
-	printf("EthernetSourceMAC: %s\n", ether_ntoa(eptr->ether_shost));
+	printf("EthernetSourceMAC:             %s\n", ether_ntoa(eptr->ether_shost));
 	//printf("MAC destino en la TRAMA ETHERNET: %s\n", ether_ntoa(eptr−>ether_dhost));
-	printf("EthernetDestinationMAC: %s\n",ether_ntoa(eptr->ether_dhost));
+	printf("EthernetDestinationMAC:        %s\n",ether_ntoa(eptr->ether_dhost));
 
-	//ahora examino datos del payload (en este caso es ARP)
-	
+	//ahora examino datos del payload (en este caso es ARP por el filtro)
+	//compruebo que sea ARP
+	if(ntohs(eptr->ether_type)!=ETHERTYPE_ARP){
+		printf("No viaja ARP sobre esta trama (aunque ya esta filtrada...)\n");
+		return -1;
+	}
+	//else...
+	puts("vamos con el ARP\n");	
+	struct ether_arp *arpPtr;
+	//ahora posiciono el puntero en el primer byte(es decir con un offset de size of ether header)
+	arpPtr =(struct ether_arp*)(packet+sizeof(struct ether_header));//o lo que es lo mismo packet+14;
+	//ahorita, muestro la info que tiene la estructura esta para ARP:
+	fprintf(stdout,"ARP: IP Origen: %d.%d.%d.%d\n",arpPtr->arp_spa[0],arpPtr->arp_spa[1],arpPtr->arp_spa[2],arpPtr->arp_spa[3]);
+	fprintf(stdout,"ARP: IP Destino: %d.%d.%d.%d\n",arpPtr->arp_tpa[0],arpPtr->arp_tpa[1],arpPtr->arp_tpa[2],arpPtr->arp_tpa[3]);
+	printf("ARP: MAC Origen:               %s\n",ether_ntoa(arpPtr->arp_sha));
+	printf("ARP: MAC Destino:              %s\n",ether_ntoa(arpPtr->arp_tha));
 
 	//Lo almacene en la tabla de dialogos =)
 	count++;
@@ -209,8 +224,9 @@ int main(int argc, char *argv[]){
 				fprintf(stderr,"Error aplicando el filtro\n");
 				exit(1);
 			}
-			pcap_loop(descr,-1,my_callback,NULL);//entra en el bucle infinito
-
+			//a continuacion capturamos, en este caso en bucle infinito
+			pcap_loop(descr,-1,my_callback,NULL);//OJO, asegurar esta linea con algun if como los anteriores
+	
 			_exit(EXIT_SUCCESS);
 
 	}//FIN DEL FORK PARA ARPCOLLECTOR
