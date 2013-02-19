68be91e8931ff2930cca68bbdd020d62e46fe9cb
silvestrefigueroa@gmail.com <silvestrefigueroa@gmail.com@037a9b29-60ad-6186-cfe9-0938512c29f3>
2013-02-19 21:31:18 +0000

    comentado el primer fork, comentada la parte de escritura en el pipe, limpiado de warnings, compila, corre, se clava en 1.2MB y no se cuelga... sigue y sigue.. a ver como hacemos ahora para hacer ..todo en uno..

--- arpCollector_callback.c
+++ arpCollector_callback.c
@@ -100,14 +100,16 @@ void arpCollector_callback(arpCCArgs args[],const struct pcap_pkthdr* pkthdr,con
 		strcpy(paquete+strlen(paquete),inet_ntoa(*(struct in_addr *) arpPtr->arp_spa));
 		strcpy(paquete+strlen(paquete),"|");//arpDstIp=");
 		strcpy(paquete+strlen(paquete),inet_ntoa(*(struct in_addr *) arpPtr->arp_tpa));
+
+
+		/*
 		//escribo en el pipe...
-	//	sem_wait((sem_t*) & (args[0].shmPtr[0].semaforo));//bloquea perfecto
 
 		if(!write((int)args[0].fdPipe[1], paquete, strlen(paquete))){
 					perror("write()");
 					_exit(EXIT_FAILURE);
 		}
-
+		*/
 		//printf("\nLa funcion CALLBACK ha escrito en el PIPE: %s\n",paquete);
 		//puedo continuar con el proximo =) finaliza la tarea de la Callback
 		//aumenta el contador de frames
