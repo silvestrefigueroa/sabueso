28832079c28371e8fb2b873033983ee5cc8096f7
silvestrefigueroa@gmail.com <silvestrefigueroa@gmail.com@037a9b29-60ad-6186-cfe9-0938512c29f3>
2013-02-22 02:52:48 +0000

    comparacion funcionando perfectamente con strcmp, verificaciones triviales desde el arpCollector... luego falta que almacene en la tabla.. para ello debera comprobar cada entrada.. y eso..OJO que hay que ver si se traba esperando tener lugar en la tabla.. que no jorobe a las otras capturas.. falta depurar eso.. como se ejecuta la callback realmente?? es todo estructurado o es orientada a eventos la llamada que hace pcap??

--- arpCollector_callback.c
+++ arpCollector_callback.c
@@ -111,17 +111,17 @@ void arpCollector_callback(arpCCArgs args[],const struct pcap_pkthdr* pkthdr,con
 		close(args[0].fdPipe[0]);
 
 		printf("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n");
-//		int srcMacEquals=1;//coinciden por default
-//		char* broadcastMac="ff:ff:ff:ff:ff:ff";//Estara mal? deberia inicializar a null y luego cargarle la cadena?deberia reservar?
-//		char* zeroMac="0:0:0:0:0:0";//lo mismo que el anterior
-//		int doCheckIpI=0;
-//		int doCheckSpoofer=0;
+		int srcMacEquals=1;//coinciden por default
+		char* broadcastMac="ff:ff:ff:ff:ff:ff";//Estara mal? deberia inicializar a null y luego cargarle la cadena?deberia reservar?
+		char* zeroMac="0:0:0:0:0:0";//lo mismo que el anterior
+		int doCheckIpI=0;
+		int doCheckSpoofer=0;
 	//	int doHitIncrement=0;
-//		int nextState=0;//por default, almacenarla y ya
-//		char* type=NULL;//consultar posibles valores en tabla_de_dialogos.txt [Arquitectura]
+		int nextState=0;//por default, almacenarla y ya
+		char* type=NULL;//consultar posibles valores en tabla_de_dialogos.txt [Arquitectura]
 //		int i=0;
-//		int dstZeroMacFlag=0;
-//		int dstBrdMacFlag=0;
+		int dstZeroMacFlag=0;
+		int dstBrdMacFlag=0;
 
 		//mapear los datos para facilitar la manipulacion de los mismos y el codeado.
 //		ethSrcMac=ether_ntoa((const struct ether_addr*) eptr->ether_shost);
@@ -135,30 +135,37 @@ void arpCollector_callback(arpCCArgs args[],const struct pcap_pkthdr* pkthdr,con
 		//Ahora como minimo reviso consistencias menores en la trama y el mensaje ARP
 //pasted code start here
 		if(*ethSrcMac!=*arpSrcMac){
-			printf("LOG:se ha detectado inconsistencia entre la MAC origen de la trama y la MAC origen del mensaje ARP\n");//podria ser proxyARP???
+			printf("LOG:se ha detectado inconsistencia entre la MAC origen de la trama y la MAC origen del mensaje ARP\n");
+			//podria haber sido proxyARP???
 			printf("LOG:Son realmente distintos %s y %s  ??\n",ethSrcMac,arpSrcMac);
 			//desde ya establezco que la trama es inconsistente en la direccion MAC de origen
-//			srcMacEquals=0;//ya que por default coinciden...
+			srcMacEquals=0;//ya que por default coinciden...
 		}
-		else{//nada... esta ok punto.
+		else{//si en lugar de no coincidir, viene como es esperable...
 			printf("LOG:ethSrcMac=arpSrcMac     OK\n");
+			srcMacEquals=1;//aunque por default coinciden
 		}
-
-		/*
-		if(*ethDstMac!=*arpDstMac){
-			printf("LOG: POR ENTRAR AQUI SE QUE SON MAC DESTINO DISTINTAS\n");
-			//si difieren en la MAC destino pero es el caso particular del broadcast, entonces me aseguro!!
-			printf("LOG:entonces %s es distinto de %s\n",ethDstMac,arpDstMac);
-			//puts("siguio...\n");
-			//printf("LOG:aaaaaaaaaaa tengo: %s y %s \n\n",ethDstMac,"ff:ff:ff:ff:ff:ff");
-			if(*ethDstMac==*broadcastMac){
+		if(*ethSrcMac==*arpSrcMac){
+			printf("LOG: LAS MAC ORIGEN SON IGUALES por compracion con == \n");
+		}
+		if(!strcmp(ethSrcMac,arpSrcMac)){
+			printf("LOG las mac origen son iguales por strcmp\n");
+		}
+		printf("------sigue-----\n");
+		if(strcmp(ethDstMac,arpDstMac)){
+			printf("LOG: las mac destino son DISTINTAS por strcmp\n");
+			//codigo para cuando son distintas aqui...
+//			if(*ethDstMac==*broadcastMac){//old bad mode
+			if(!strcmp(ethDstMac,broadcastMac)){
 				dstBrdMacFlag=1;
-				puts("LOG:ethDsrMac es broadcast!!!\n");
+				puts("LOG:ethDsrMac es broadcast por strcmp!!!\n");
 				//mmm iba al broadcast, sera una pregunta realmente? o sera para engañar?
-				if(*arpDstMac==*zeroMac){//si es una pregunta ARP, lo marco para consultar su credibilidad? o consulto yo?
-					//OK, es ARP request (al menos por la formacion)
-					//es al menos una trama aceptable, podria verificarse luego pero al menos la acepto asi!
-					//verifico si la IP de destino coincide con la del host que tiene la MAC ethDstMac
+//				if(*arpDstMac==*zeroMac){//old bad mode
+				if(!strcmp(arpDstMac,zeroMac)){//si devuelve 0 son iguales :)
+					//si es una pregunta ARP, lo marco para consultar su credibilidad? o consulto yo en db conocimiento?
+					//OK, tiene la arquitectura de ARP request/question
+					//es al menos una trama aceptable, podria verificarse luego pero al menos la guardo
+					//verifico si la IP de destino coincide con la del host que tiene la MAC ethDstMac en dbconocimiento
 					//si quiero realmente probar esto, deberia chequear los pares MACIP de cada host participante
 					dstZeroMacFlag=1;
 					printf("LOG:puede que sea una pregunta ARP legitima..\n");//faltaria verificar match de ip-mac origen.
@@ -166,27 +173,31 @@ void arpCollector_callback(arpCCArgs args[],const struct pcap_pkthdr* pkthdr,con
 					doCheckIpI=1;
 					doCheckSpoofer=1;
 					nextState=1;
-					type="PASS";
+					type="PASS";//deberian ser un macro de variable entero y ya..
+
 					printf("Finalizada la evaluacion, continua con la carga de datos...\n");
 				}
 				else{//Si entra aqui, es porque fue al broadcast, pero el ARP tiene un destino FIJO, es muy extraño!!
 					printf("LOG:caso extraño, ethDstMac broadcast y arpDstMac Unicast...anomalo!!\n\n");
-					//podria verificar el match IP-MAC origen, es un caso para WARN no para evaluarlo porque no viene al caso por ahora.
-					//WARNING: deberia comprobarla completamente antes de informar..pero excede los limites del trabajo final
-						//He decidido activar el flag type en WARN y no tratar el problema pero si mostrarlo!!
+					//podria verificar el match IP-MAC origen, es un caso para WARN no para evaluarlo 
+					//He decidido activar el flag type en WARN y no tratar el problema pero si mostrarlo!!
+					type="WARN";//deberian ser un macro de variable entero y ya..
 					
 				}
 			}
-			else{//para los casos que no son broadcast en ethernet
-
+			else{
+				printf("no estaba dirigido al broadcast ethernet, es UNICAST :p\n");
+				//bueno aqui va el codigo para cuando estaba la trama dirigida a una mac especifica:
 				//destino ethernet bien definido, pero MAC destino en ARP DISTINA!!MALFORMACION!!
 				//Este curioso caso se da por ejemplo con el DDwrt. el destino en ARP debera ser 0:0:0:0:0:0
 				printf("LOG:antes de comparar con zero, tengo %s y %s\n",arpDstMac,zeroMac);
-				if(*arpDstMac==*zeroMac){
+//				if(*arpDstMac==*zeroMac){//bad old style
+				if(!strcmp(arpDstMac,zeroMac)){
+					printf("LOG: por strcmp, mac destino en ARP es todo 0:  %s\n",arpDstMac);
 					dstZeroMacFlag=1;
 					//es altamente probable que sea una preguntita del AP que se hace el que no sabe quien es el cliente
 					//para confirmar, valido ethSrcMac con arpSrcMac y luego arpSrcMac con arpSrcIp =)
-					printf("LOG:Posible mensaje del AP, compruebe que ethSrcMac matchea con arpSrcIp para descartar ataque DoS\n");
+					printf("LOG:Posible mensaje del AP,compruebe que ethSrcMac matchea con arpSrcIp para descartar DoS\n");
 					//tratar el error o escapar si OK
 					//WARNING, marcar para comprobar y almacenar.
 					//Escapa del formato de arpspoofing estudiado, me limito a mostrar el WARN, se descarta la trama
@@ -195,7 +206,7 @@ void arpCollector_callback(arpCCArgs args[],const struct pcap_pkthdr* pkthdr,con
 				}
 				//el else de abajo OJO, porque queda el resto en el que las 4 mac son iguales!!
 				else{//se trata de MACs destinos AMBIGUOS, es una trama anomala!! a no ser que sea del proxyARP
-					printf("LOG:Trama con destino definido, revisando en profundidad....Posible ProxyARP\n\n");
+					printf("LOG:por strcmp, Trama con destino definido, revisando en profundidad....Posible ProxyARP\n\n");
 					//No es el caso analizado, se descarta la trama pero se indica el WARN
 					type="WARN";
 					nextState=0;
@@ -203,49 +214,50 @@ void arpCollector_callback(arpCCArgs args[],const struct pcap_pkthdr* pkthdr,con
 				}
 			}
 		}
-		else{//macs destino coinciden, o sea bien dirigido..puede ser una trampa, si el origen tiene spoofeada la IP es la trama del atacante
+		else{
+			printf("LOG: mac destino IGUALES por strcmp, sera el caso de un mensaje respuesta ARP??\n");
+			//aqui el codigo para cuando las mac destino son IGUALES:
+			//macs destino coinciden, o sea bien dirigido
+			//puede ser una trampa, si el origen tiene spoofeada la IP es la trama del atacante
 			//o bien son tramas ARP que cayeron en el filtro (y vienen del portstealing) pero spoofeadas tambien por que no?
 			//primero que nada chekeo si las MAC origen son iguales (primer verificacion, leo el resultado directamente)
-				//si son iguales, veo el match MAC-IP del origen para ver si es ataque (consulto info real)
-				switch(srcMacEquals){//lo puse en switch porque podria ser casos especiales de MAC Reservadas, 
-							//de momento funciona igual q con IF-else
-					case 1:
+			//si son iguales, veo el match MAC-IP del origen para ver si es ataque (consulto info real)
+			
+			switch(srcMacEquals){//lo puse en switch porque podria ser casos especiales de MAC Reservadas, 
+						//de momento funciona igual q con IF-else
+				case 1:
 						//trama OK, debera verificar capa de red IP
-							//si no matchea, entonces ALERTO EL ATAQUE!!!
-							//SI MATCHEA, tenemos origen OK, destino OK.... nada raro.. me robe un ARP..
-							printf("LOG:[Taxonomia de respuesta o ATAQUE], par[%s]-[%s]\n",ethDstMac,arpDstMac);
-							//marcar para portstelear y GUARDAR el dialogo en la tabla
-							doCheckIpI=1;//siempre primero, es la trivial.si conozco la info real, no noecesito el stealer.
-							doCheckSpoofer=1;
-							type="PASS";
-							nextState=1;
-							//Normalmente a no ser que sea una respuesta dirigida al sabueso, no veria estas tramas...
-							//es por ello que lo mas seguro es que esta trama sean robadas del porstealing
-					break;
-					case 0:
-							//no son iguales las MAC origen
-							//Puede ser proxyARP????(ojo que esta filtrado) o bien 
-								//el origen (sender) esta haciendo algo raro
-							//WARNING-> inconsistencia en las MAC origen
-							printf("LOG:macs origen no coinciden, posible proxyARP o trama anomala\n");
-							type="WARN";
-							nextState=2;
-					break;
-					default:
-						printf("LOG:caso anomalo no tratado, no pudo determinarse igualdad de mac origen\n");
-						//en estos casos, podria meter en la primer evaluacion respecto a las srcMac, numero superiores
-						//para casos especiales, de momento no se trata este tipo de "mac reservada"
-					break;
-				}
-		}
-		*/
-
-
-
-
-
+						//si no matchea, entonces ALERTO EL ATAQUE!!!
+						//SI MATCHEA, tenemos origen OK, destino OK.... nada raro.. me robe un ARP..
+						printf("LOG:[Taxonomia de respuesta o ATAQUE], par[%s]-[%s]\n",ethDstMac,arpDstMac);
+						//marcar para portstelear y GUARDAR el dialogo en la tabla
+						doCheckIpI=1;//siempre primero, es la trivial.si conozco la info real, no noecesito el stealer.
+						doCheckSpoofer=1;
+						type="PASS";
+						nextState=1;
+						//Normalmente a no ser que sea una respuesta dirigida al sabueso, no veria estas tramas...
+						//es por ello que lo mas seguro es que esta trama sean robadas del porstealing
+				break;
+				case 0:
+						//no son iguales las MAC origen
+						//Puede ser proxyARP????(ojo que esta filtrado) o bien 
+							//el origen (sender) esta haciendo algo raro
+						//WARNING-> inconsistencia en las MAC origen
+						printf("LOG:macs origen no coinciden, posible proxyARP o trama anomala\n");
+						type="WARN";
+						nextState=2;
+				break;
+				default:
+					printf("LOG:caso anomalo no tratado, no pudo determinarse igualdad de mac origen\n");
+					//en estos casos, podria meter en la primer evaluacion respecto a las srcMac, numero superiores
+					//para casos especiales, de momento no se trata este tipo de "mac reservada"
+				break;
+			}//case
+		}//else en el que son IGUALES las mac destino (viene del if de si son distintas)
 
 //pasted code finalize here
+
+
 		//puedo continuar con el proximo =) finaliza la tarea de la Callback
 		//aumenta el contador de frames
 		count++;
