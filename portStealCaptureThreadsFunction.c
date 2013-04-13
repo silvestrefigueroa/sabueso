8a08f9eb3b74bc45dbffc31b9cd99f1af8d4632a
silvestrefigueroa@gmail.com <silvestrefigueroa@gmail.com@037a9b29-60ad-6186-cfe9-0938512c29f3>
2013-04-13 20:20:28 +0000

    mejorado el algoritmo en el punto de lanzada del psCapturer, se añadio validacion de trama por nextState para no revisarla si su nextState no es exclusivamente = 1, se marco la trama revisada con nextState=3 (no las reviso de nuevo.. y algunas otras yerbas por el estilo. Sigo ahora intentando capturar desde el hilo pscapturer, luego chequeare y testeare portsteleando a mano y ya casiii wiii

--- portStealCaptureThreadsFunction.c
+++ portStealCaptureThreadsFunction.c
@@ -1,7 +1,71 @@
 #include <stdlib.h>
 
+#define ASKER "192.168.1.100"
+#define SERVER2GUARD "192.168.1.1"
+
 void *portStealCaptureThreadsFunction(){
-	printf("******************************************************************************funcion del hilo\n");
-	sleep(1000);
+	printf("******************************************************************************funcion del hilo psCapturer\n");
+
+
+
+	//este hilo captura... y cuanodo detecta una inconsistencia avisa
+	//si recibe una señal para detener la captura, se detiene sin mas... (podria usar semaforo para asegurarme que termine el callback)
+	
+	//PREPARAR CAPTURA:
+
+	//ahora compilo el programa de filtrado para hacer un filtro para ARP
+
+	//quiero capturar todo el trafico con origen el server2guard y destino el asker
+	//FILTRO: dst 192.168.1.1 and src 192.168.1.200 (DONDE .1 ES EL SERVER Y .200 EL ASKER POR EJ.)
+	//ARMO EL FILTRO CONCATENANDO:
+
+	char filtro[44];//es 43 pero por si le pifie le di uno mas...por el \0
+
+	memset(filtro,0,44);//inicializo
+
+	strcpy(filtro,"dst ");
+	strcpy(filtro+strlen(filtro),ASKER);
+	strcpy(filtro+strlen(filtro)," and src ");
+	strcpy(filtro+strlen(filtro),SERVER2GUARD);
+
+	printf(":::::::::::::::::::::::::::::::::::FILTRO: %s\n",filtro);
+	sleep(2);
+
+	
+/*
+        if(pcap_compile(descr,&fp,"arp",0,netp)==-1){//luego lo cambiare para filtrar SOLO los mac2guards
+                fprintf(stderr,"Error compilando el filtro\n");
+                exit(1);
+        }
+        //Para APLICAR el filtro compilado:
+        if(pcap_setfilter(descr,&fp)==-1){
+                fprintf(stderr,"Error aplicando el filtro\n");
+                exit(1);
+        }
+//Proceso de captura:
+                        puts("\n-------------------------");
+                        puts("HILO recolector de tramas ROBADAS...\n");
+
+                        //COmienza a preparar la captura...
+                        dev=NULL;
+                        net=NULL;
+                        mask=NULL;
+                        //Argumentos para la funcion callback
+                        arpCCArgs conf[2] = {
+                        //      {0, "foo",shmPtr,arpAskers_shmPtr},
+                                {tableSize, "Argumentos",shmPtr,arpAskers_shmPtr,arpAskersTable_tableSize}
+                        };
+                        //le paso los descriptores del PIPE
+                        conf[0].fdPipe[0]=fdPipe[0];
+                        conf[0].fdPipe[1]=fdPipe[1];
+                        //El bucle de captura lo armo con variables que el padre ya preparo antes cuando hizo el check de la netmask
+                        pcap_loop(descr,-1,(pcap_handler)arpCollector_callback,(u_char*) conf);
+                        _exit(EXIT_SUCCESS);
+	
+
+*/
+
+
+
 	return NULL;
 }
