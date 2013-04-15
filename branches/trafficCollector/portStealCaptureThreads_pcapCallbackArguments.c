c0ecd8d30d6a8d487e839e987cebc66cb77d3a9d
silvestrefigueroa@gmail.com <silvestrefigueroa@gmail.com@037a9b29-60ad-6186-cfe9-0938512c29f3>
2013-04-15 22:24:15 +0000

    nombre definitivo del nuevo branch... se eliminaron los portStealCapture y se procedera a modificar el arpCollector para convertirlo en un trafficCollector

--- branches/trafficCollector/portStealCaptureThreads_pcapCallbackArguments.c
+++
@@ -1,8 +0,0 @@
-//Este fichero es para poner la estructura que desde el sabueso seteo para pasar los argumentos a la funcion calback del hilo capturador de tramas robadas
-//Silvestre E. Figueroa @ FI-UM 2012-2013
-//Define here the portStealCaptureThreafsFunction_pcapCallbackArguments struct
-typedef struct {
-        int tableSize;
-        char title[255];
-        struct arpDialog* shmPtr;//puntero al array de arpDialog = seria la arpDialoguesTable
-}portSCTA_pcapCallbackArgs;
